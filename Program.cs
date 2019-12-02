991383f9a0d586cfcf024b6b95ed07ab92460707
samiayasmin828 <57412632+samiayasmin828@users.noreply.github.com>
2019-12-02 08:14:56 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+ 
+namespace PrimitiveRoot
+{
+    
+    class Program
+    {
+        static void PrimitiveRoot(int p)
+        {
+            int z = p - 1;
+            BigInteger [,] r = new BigInteger[z, z];
+            for(int i=0;i<z;i++)
+            {
+                for(int j=0;j<z;j++)
+                {
+                    r[i, j] =(BigInteger) Math.Pow(i + 1, j + 1);
+                    r[i,j]%=p;
+                }
+             }
+            HashSet<BigInteger> st = new HashSet<BigInteger>();
+            for(int i=0;i<z;i++)
+            {   
+                for(int k=0;k<z;k++){
+                    st.Add(r[i,k]);
+                }
+ 
+               if(st.Count == z){
+                    Console.Write("Primitive root : " + (i + 1) );
+                    Console.WriteLine();
+               }
+                st.Clear();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a prime number");
+            n = Convert.ToInt32(Console.ReadLine());
+            PrimitiveRoot(n);
+            Console.ReadLine();
+        }
+    }
+ 
+}
